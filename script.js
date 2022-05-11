c15c16d0987b7d5a5a9e740c18b02f0b0125b07c
Eric Hochwald <erichochwald@erics-mbp.myfiosgateway.com>
2022-05-11 12:58:41 -0400

    added pseudo code js file

---
+++ script.js
@@ -0,0 +1,9 @@
+// ask the user rock, paper, or scissors
+
+// store the user input
+
+// generate the computers r, p, or s
+
+// if user and computer prompt are the same, record a tie
+
+// else if user chose rock and computer chose 
