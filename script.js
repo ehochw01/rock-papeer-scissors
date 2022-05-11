a0b4ac2b0d6a45c82324b6dcb109f699f593975d
Eric Hochwald <erichochwald@erics-mbp.myfiosgateway.com>
2022-05-11 13:01:39 -0400

    added index.html

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
+// initialize win-loss-tie record for user and computer
+
+// initialize wantsToPlay = true
+
+// while wantsToPlay = true {
+
+// call play function
+
+// play function:
+
 // ask the user rock, paper, or scissors
 
 // store the user input
 
-// generate the computers r, p, or s
+// generate the computer's r, p, or s and record it
 
-// if user and computer prompt are the same, record a tie
+// if user and computer input is the same
+// record a tie
 
-// else if user chose rock and computer chose 
+// else if user chose rock...
+// if computer chose scissors record a user win 
+// if computer chose paper record a computer win
+
+// else if user chose scissors...
+// if computer chose paper record a user win
+// if comptuer chose rock record a computer win
+
+// else if user chose paper...
+// if computer chose rock record a user win
+// if computer chose scissors record a computer win
+
+// end play function
+
+// display win-loss-tie record
+
+// ask user if they want to play again
+// if no, set wantsToPlay = false
+
+// }
+
